28b3344e038bf7602f8af087eb493ee9bc45e226
Ezequiel Amin <ezequielamin@outlook.com>
2024-07-16 16:01:35 -0300

    configuraciones iniciales del backend de la comision 98i

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// TODO: Importar base de datos
+
+console.clear();
+console.log('⌛ Inicializando servidor...');
+
+// 1. Inicializar el servidor
+const app = express();
+
+// 2. Configurar los valores del servidor
+const PORT = process.env.PORT || 3000;
+
+// 3. Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+// 4. Rutas (o endpoints)
+// TODO: Importar rutas
+
+// 5. Loop del servidor
+app.listen(PORT, () => {
+  console.log(`🥟 El servidor está arriba y en el puerto ${PORT}`);
+});
