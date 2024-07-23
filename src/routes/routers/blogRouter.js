49e0e996f897e0b3d7bcee08ebd3985951fcf430
Ezequiel Amin <ezequielamin@outlook.com>
2024-07-23 16:03:01 -0300

    agrego validaciones post y put

--- src/routes/routers/blogRouter.js
+++ src/routes/routers/blogRouter.js
@@ -1,5 +1,8 @@
 import express from 'express';
+
 import { Blogs } from '../../controllers/blogs/index.js';
+import { validateBody } from '../../middlewares/validateBody.js';
+import { post_put_blogValidationSchema } from '../../helpers/validationSchemas/blogsValidationSchemas.js';
 
 export const blogRouter = express.Router();
 
@@ -8,10 +11,20 @@ blogRouter.get('/', Blogs.GetController.getBlogs);
 
 // POST ----------------------------
 // /api/v1/blogs/
-blogRouter.post('/', Blogs.PostController.postBlog);
+blogRouter.post(
+  '/',
+  (req, res, next) =>
+    validateBody(req, res, next, post_put_blogValidationSchema),
+  Blogs.PostController.postBlog,
+);
 
 // PUT ----------------------------
-blogRouter.put('/:id', Blogs.PutController.putBlog);
+blogRouter.put(
+  '/:id',
+  (req, res, next) =>
+    validateBody(req, res, next, post_put_blogValidationSchema),
+  Blogs.PutController.putBlog,
+);
 
 // DELETE -------------------------
 blogRouter.delete('/:id', Blogs.DeleteController.deleteBlog);
